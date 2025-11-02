33a09605960a7c1d1b66adad70117535e7fb77c8
varshini-balakrishnan-ics4u1-01-25 <varshini.balakrishnan@student.tdsb.on.ca>
2025-11-02 19:18:35 +0000

    update PracticeProbkem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,12 +1,16 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		int[] arr = {1,2,3};
+		System.out.println(validIndex(arr,2)); // true
+		System.out.println(validIndex(arr, 5)); // false? (yeah false)
+		System.out.println(divide(10,2)); //10 div 2 = five (5)
+		System.out.println(divide(10,0)); //0
 
 	}
 
 	public static boolean validIndex (int[] arr, int index) {
 		try {
-			arr[index] = arr[index];
 			return true;
 		} catch (Exception e) {
 			return false;
@@ -15,14 +19,7 @@ public class PracticeProblem {
 	
 	public static int divide (int a, int b) {
 		try {
-			return a / b;
-		} catch (Exception e) {
-			return 0;
-		}
-	}
-	public static int safeconvertStringtoInt(String str) {
-		try {
-			return Integer.parseInt(str);
+			return a/b;
 		} catch (Exception e) {
 			return 0;
 		}
